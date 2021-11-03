c6416a012d288e7df354b09c23956ebb1f19de31
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-03 10:15:55 -0300

    get, post, delete and update routes

--- src/routes/ProductsRoute.js
+++ src/routes/ProductsRoute.js
@@ -1,7 +1,11 @@
-const { Router } = require('express')
+const { Router } = require("express");
 const ProductsController = require("../controllers/ProductsController");
 const router = Router();
 
-router.get("/products", ProductsController.getProducts)
+router.get("/products", ProductsController.getProducts);
+router.get("/products/:id", ProductsController.getOneProduct);
+router.post("/products", ProductsController.postProduct);
+router.put("/products/:id", ProductsController.putProduct);
+router.delete("/products/:id", ProductsController.deleteProducts);
 
 module.exports = router;
