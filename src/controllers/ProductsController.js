4b69213cc91d0a877f75fc2e423b9f546e767cd2
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-03 10:14:52 -0300

    feat: get, post, put, delete controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -9,6 +9,54 @@ class ProductsController {
       return res.status(500).json(err.message);
     }
   }
+
+  static async getOneProduct(req, res) {
+    const { id } = req.params;
+    try {
+      const oneProduct = await database.Products.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(oneProduct);
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+
+  static async postProduct(req, res) {
+    const newProduct = req.body;
+    try {
+      const postCreated = await database.Products.create(newProduct);
+      return res.status(200).json(postCreated);
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+
+  static async putProduct(req, res) {
+    const { id } = req.params;
+    const updateProduct = req.body;
+    try {
+      await database.Products.update(updateProduct, {
+        where: { id: Number(id) },
+      });
+      const productUpdated = await database.Products.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(productUpdated);
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+
+  static async deleteProducts(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Products.destroy({where: { id: Number(id) }});
+      return res.status(200).json({message: `Product ${id} was successfully deleted`});
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
 }
 
 module.exports = ProductsController;
