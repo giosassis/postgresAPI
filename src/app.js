235d2e49a4be5a7100a03094cd3552134ef3fefc
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-03 08:37:21 -0300

    feat: routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const routes = require('./routes/router')
 
 app.use(cors());
 app.use(express.json())
+app.listen(3001, () => console.log("Server is running on port 3000"))
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+routes(app)
+
+module.exports = app;
